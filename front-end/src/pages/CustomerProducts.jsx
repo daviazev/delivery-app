031b1f4883f117449a0a904f1563de42a6a24bba
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-03-01 22:17:41 +0100

    feat: desenvolvimento da tela de produtos

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Products from '../components/Products';
 
 export default function CustomerProducts() {
   return (
     <div>
       <Navbar />
+      <Products />
     </div>
   );
 }
