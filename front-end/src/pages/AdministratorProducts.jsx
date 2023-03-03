eaccbe7fc79d549e05ef131863f93794653f223c
daviazevdo <azvd.davi@gmail.com>
2023-03-03 17:22:18 -0300

    feat: desenvolvimento inicial da tela de pessoa administradora

--- front-end/src/pages/AdministratorProducts.jsx
+++ front-end/src/pages/AdministratorProducts.jsx
@@ -2,6 +2,50 @@ import React from 'react';
 
 export default function AdministratorProducts() {
   return (
-    <div>AdministratorProducts</div>
+    <div>
+      <nav>
+        <ul>
+          <li
+            data-testid="customer_products__element-navbar-link-orders"
+          >
+            GERENCIAR USUÁRIOS
+
+          </li>
+          <li
+            data-testid="customer_products__element-navbar-user-full-name"
+          >
+            NOME DO USUÁRIO
+
+          </li>
+          <li data-testid="customer_products__element-navbar-link-logout">SAIR</li>
+        </ul>
+      </nav>
+      <section>
+        <input
+          data-testid="admin_manage__input-name"
+          type="text"
+        />
+        <input
+          type="email"
+          data-testid="admin_manage__input-email"
+        />
+        <input
+          type="password"
+          data-testid="admin_manage__input-password"
+        />
+        <select data-testid="admin_manage__select-role">
+          <option>Vendedor</option>
+          <option>Cliente</option>
+          <option>Administrador</option>
+        </select>
+      </section>
+      <button
+        type="button"
+        data-testid="admin_manage__button-register"
+      >
+        CADASTRAR
+
+      </button>
+    </div>
   );
 }
