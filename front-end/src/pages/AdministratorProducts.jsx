1a31377c4c13bf657cb50bceb8f8d744b86d752b
daviazevdo <azvd.davi@gmail.com>
2023-03-03 17:49:08 -0300

    feat: definição do estado e criação de validação dos dados inseridos

--- front-end/src/pages/AdministratorProducts.jsx
+++ front-end/src/pages/AdministratorProducts.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { checkEmailAndPassword, checkUser } from '../utils/checkUser';
 
 export default function AdministratorProducts() {
+  const [user, setUser] = useState({ email: '', name: '', password: '', role: '' });
+  const [isDisable, setIsDisable] = useState(true);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  useEffect(() => {
+    const { email, password, name } = user;
+    const checkPasswordAndEmail = checkEmailAndPassword(email, password);
+    const checkName = checkUser(name);
+
+    if (checkName && checkPasswordAndEmail) setIsDisable(false);
+    else { setIsDisable(true); }
+  }, [user]);
+
   return (
     <div>
       <nav>
@@ -24,16 +41,29 @@ export default function AdministratorProducts() {
         <input
           data-testid="admin_manage__input-name"
           type="text"
+          onChange={ handleChange }
+          placeholder="Nome e sobrenome"
+          name="name"
         />
         <input
           type="email"
           data-testid="admin_manage__input-email"
+          onChange={ handleChange }
+          placeholder="Email"
+          name="email"
         />
         <input
           type="password"
           data-testid="admin_manage__input-password"
+          onChange={ handleChange }
+          placeholder="password"
+          name="password"
         />
-        <select data-testid="admin_manage__select-role">
+        <select
+          onChange={ handleChange }
+          data-testid="admin_manage__select-role"
+          name="role"
+        >
           <option>Vendedor</option>
           <option>Cliente</option>
           <option>Administrador</option>
@@ -42,6 +72,7 @@ export default function AdministratorProducts() {
       <button
         type="button"
         data-testid="admin_manage__button-register"
+        disabled={ isDisable }
       >
         CADASTRAR
 
